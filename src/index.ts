774f5ee73f9d9f1d176d300290c5892855b0122a
interc0der <intercoder@whirled.io>
2022-06-28 03:57:43 -0700

    Update: add starter script for testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-const main = () => {};
+import Client, { constants } from '@thebettermint/xrpl-tx-parser';
+import config from '../config.json';
+import walletService from './services/wallet.service';
+import balanceService from './services/balance.service';
+
+const main = async () => {
+  let api = new Client({
+    registry: config[0].registry || undefined,
+  });
+  let events = constants.wsStatusMessages;
+
+  api.once(events.connected, () => {
+    console.log(events.connected);
+  });
+
+  api.on(events.tx, (e: any) => {
+    balanceService.process(e);
+  });
+
+  api.on(events.timeout, () => {
+    console.log('timeout');
+  });
+
+  api.on(events.reconnect, () => {
+    console.log('reconnect');
+  });
+
+  api.once(events.closed, () => {
+    console.log(events.closed);
+  });
+};
 
 main();
