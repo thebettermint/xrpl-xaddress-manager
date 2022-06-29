5df788c7d2b655098fd7c815ba2f2d1777174242
interc0der <intercoder@whirled.io>
2022-06-29 09:38:10 -0700

    Remove: delete unnecessary types for compile

--- types/parser/account.ts
+++
@@ -1,9 +0,0 @@
-export interface AccountsInterface {
-  account?: string;
-  tx_result?: string;
-  tx_type?: string;
-  time?: number;
-  ledger_index?: number;
-  tx_index?: number;
-  tx_hash?: string;
-}
